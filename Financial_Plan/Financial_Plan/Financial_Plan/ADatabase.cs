5d7c67d32d042ee8a653cdcfa8a88880cbb5e76f
Benedikt Oleksik <benedikt.oleksik@hotmail.de>
2016-06-30 08:58:31 +0200

    Add SQL Chema

--- Financial_Plan/Financial_Plan/Financial_Plan/ADatabase.cs
+++ Financial_Plan/Financial_Plan/Financial_Plan/ADatabase.cs
@@ -8,6 +8,22 @@ namespace Financial_Plan
 {
     public abstract class ADatabase
     {
+        protected const string CREATE_NEW_DATABASE = @"CREATE SCHEMA Financials;
+                                                        USE Financials;
+                                                        CREATE TABLE User (
+                                                            pkID SERIAL PRIMARY KEY,
+                                                            name TEXT DEFAULT 'default'
+                                                        );
+                                                        INSERT INTO User
+                                                            (name)
+                                                        VALUES
+                                                            ('default');
+                                                        CREATE TABLE FinanzItems (
+                                                            pkID SERIAL PRIMARY KEY,
+                                                            userID BIGINT UNSIGNED FOREIGN KEY User.pkID,
+                                                            type INT default 0,
+                                                            val DOUBLE
+                                                        );";
         protected const string SELECT_FINANCIES = "";
         protected const string INSERT_FINANCIES = "";
         private string DATABASE_PW;
